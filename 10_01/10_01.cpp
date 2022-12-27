e37bd3e0617521013ddbd8f927d878936ffc12c3
Terentev Dmitry <116565548+TerentevDmitry@users.noreply.github.com>
2022-12-27 13:07:39 +0300

    v1

--- 10_01/10_01.cpp
+++ 10_01/10_01.cpp
@@ -1,8 +1,20 @@
 ﻿// 10_01.cpp : Задача 1. Простейший проект
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian"); //Корректное отображение Кириллицы
+	system("chcp 1251");
+	std::cout << std::endl;
+
+	std::string name;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+
+	std::cout << "Здравствуйте, " << name << "!";
+	std::cout << std::endl;
+
 }
